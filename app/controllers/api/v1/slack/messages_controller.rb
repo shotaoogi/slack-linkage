7cea007e233e2f2984f5ff8a4edb29728bd6cc52
shotaoogi <shota.oogi@gmail.com>
2021-12-12 15:57:01 +0900

    Add slack messages index

--- app/controllers/api/v1/slack/messages_controller.rb
+++ app/controllers/api/v1/slack/messages_controller.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
-class Slack::MessagesController < ApplicationController
+class Api::V1::Slack::MessagesController < Api::V1::ApplicationController
+  skip_before_action :logged_in_user
+
+  def index
+    render json: 'ok'
+  end
+
   def create
     @body = JSON.parse(request.body.read)
     case @body['type']
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: 'slack/messages#index'
   namespace :api do
     namespace :v1 do
       post '/signup', to: 'registrations#signup'
@@ -8,9 +9,9 @@ Rails.application.routes.draw do
       post '/login', to: 'sessions#create'
       delete '/logout', to: 'sessions#destroy'
       post '/logged_in', to: 'sessions#logged_in?'
+      namespace :slack do
+        resources :messages, except: %i[create]
+      end
     end
-  end
-  namespace :slack do
-    resources :messages, %i[create]
   end
 end
