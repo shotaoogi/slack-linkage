6958784b5c4483259bd3de47440d2d5fa907dd0e
shotaoogi <shota.oogi@gmail.com>
2021-12-12 19:43:58 +0900

    Hide pidfile

--- config/puma.rb
+++ config/puma.rb
@@ -5,7 +5,7 @@ min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 port        ENV.fetch("PORT") { 3001 }
 environment ENV.fetch("RAILS_ENV") { ENV['RACK_ENV'] || "production" }
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+# pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 preload_app!
 plugin :tmp_restart
