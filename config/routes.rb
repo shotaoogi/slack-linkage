c8b39bd9d5f8e4d7ea9e333b668a5e539d0ce486
shotaoogi <shota.oogi@gmail.com>
2021-12-29 22:30:59 +0900

    Change logged_in routing

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
       post '/login', to: 'sessions#create'
       delete '/logout', to: 'sessions#destroy'
-      post '/logged_in', to: 'sessions#logged_in?'
+      get '/logged_in', to: 'sessions#logged_in?'
       namespace :slack do
         resources :messages, only: %i[index] do
           collection do
