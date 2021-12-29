a1222005338dfa7af95f0c97a649f8033fae0ac9
shotaoogi <shota.oogi@gmail.com>
2021-12-29 13:31:13 +0900

    Implement test modal action

--- app/controllers/api/v1/slack/messages_controller.rb
+++ app/controllers/api/v1/slack/messages_controller.rb
@@ -8,13 +8,77 @@ class Api::V1::Slack::MessagesController < Api::V1::ApplicationController
   end
 
   def receive
-    case params[:type]
-    when 'url_verification'
-      Rails.logger.info "Sending challenge to Slack"
-    render status: 200, json: { challenge: params[:challenge] }
+    payload = JSON.parse(params[:payload])
+    case payload['type']
+    when 'shortcut'
+      client.views_open(trigger_id: payload['trigger_id'], view: build_initial_view)
+    when 'view_submission'
+      client.chat_postMessage(text: 'ok', channel: 'general')
     end
-    Rails.logger.info "Sending shortcut modal"
-    response = Slack::Events::Request.new(params)
-    render status: 200, json: { body: response.body}
+  end
+
+  private
+
+  def client
+    @client ||= Slack::Web::Client.new
+  end
+
+  def build_initial_view
+    {
+      "type": "modal",
+      "title": {
+        "type": "plain_text",
+        "text": "My App",
+        "emoji": true
+      },
+      "submit": {
+        "type": "plain_text",
+        "text": "Submit",
+        "emoji": true
+      },
+      "close": {
+        "type": "plain_text",
+        "text": "Cancel",
+        "emoji": true
+      },
+      "blocks": [
+        {
+          "type": "input",
+          "block_id": "select_user",
+          "element": {
+            "type": "static_select",
+            "action_id": "selected_user",
+            "placeholder": {
+              "type": "plain_text",
+              "text": "Select a user",
+              "emoji": true
+            },
+            "options": [
+              {
+                "text": {
+                  "type": "plain_text",
+                  "text": "AAA",
+                  "emoji": true
+                },
+                "value": "selected_aaa"
+              },
+              {
+                "text": {
+                  "type": "plain_text",
+                  "text": "BBB",
+                  "emoji": true
+                },
+                "value": "selected_bbb"
+              }
+            ]
+          },
+          "label": {
+            "type": "plain_text",
+            "text": "Label",
+            "emoji": true
+          }
+        }
+      ]
+    }
   end
 end
